9a0bc3e9fd6889f0af761d7d69bfb82f7b6bd7b0
Chisenon <kamiru.darkness.contractor@gmail.com>
2025-11-27 14:57:55 +0900

    ♻️ Refactor CustomInspector to implement decal properties and dynamic UI for decal management

--- Editor/CustomInspector.cs
+++ Editor/CustomInspector.cs
@@ -4,27 +4,68 @@ using UnityEngine;
 
 namespace lilToon
 {
-    public class TemplateFullInspector : lilToonInspector
+    public class lilNoneInspector : lilToonInspector
     {
-        // Custom properties
-        //MaterialProperty customVariable;
+        // Decal Count
+        MaterialProperty decalCount;
+        
+        // Decal properties arrays (max 10 decals)
+        private const int MAX_DECALS = 10;
+        MaterialProperty[] useDecal = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalColor = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalTex = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalTex_ST = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalTex_SR = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalTexAngle = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalDecalAnimation = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalDecalSubParam = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalIsDecal = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalIsLeftOnly = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalIsRightOnly = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalShouldCopy = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalShouldFlipMirror = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalShouldFlipCopy = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalBlendMode = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalTex_UVMode = new MaterialProperty[MAX_DECALS];
+        MaterialProperty[] decalSyncScale = new MaterialProperty[MAX_DECALS];
 
-        private static bool isShowCustomProperties;
-        private const string shaderName = "TemplateFull";
+        private static bool[] isShowDecal = new bool[MAX_DECALS];
+        private const string shaderName = "ChiseNote/lilNone";
 
         protected override void LoadCustomProperties(MaterialProperty[] props, Material material)
         {
+
             isCustomShader = true;
 
             // If you want to change rendering modes in the editor, specify the shader here
             ReplaceToCustomShaders();
             isShowRenderMode = !material.shader.name.Contains("Optional");
 
-            // If not, set isShowRenderMode to false
-            //isShowRenderMode = false;
+            // Load Decal Count
+            decalCount = FindProperty("_DecalCount", props);
 
-            //LoadCustomLanguage("");
-            //customVariable = FindProperty("_CustomVariable", props);
+            // Load all decal properties (1-10)
+            for(int i = 0; i < MAX_DECALS; i++)
+            {
+                int num = i + 1;
+                useDecal[i] = FindProperty($"_UseDecal{num}", props);
+                decalColor[i] = FindProperty($"_Decal{num}Color", props);
+                decalTex[i] = FindProperty($"_Decal{num}Tex", props);
+                decalTex_ST[i] = FindProperty($"_Decal{num}Tex_ST", props);
+                decalTex_SR[i] = FindProperty($"_Decal{num}Tex_SR", props);
+                decalTexAngle[i] = FindProperty($"_Decal{num}TexAngle", props);
+                decalDecalAnimation[i] = FindProperty($"_Decal{num}DecalAnimation", props);
+                decalDecalSubParam[i] = FindProperty($"_Decal{num}DecalSubParam", props);
+                decalIsDecal[i] = FindProperty($"_Decal{num}IsDecal", props);
+                decalIsLeftOnly[i] = FindProperty($"_Decal{num}IsLeftOnly", props);
+                decalIsRightOnly[i] = FindProperty($"_Decal{num}IsRightOnly", props);
+                decalShouldCopy[i] = FindProperty($"_Decal{num}ShouldCopy", props);
+                decalShouldFlipMirror[i] = FindProperty($"_Decal{num}ShouldFlipMirror", props);
+                decalShouldFlipCopy[i] = FindProperty($"_Decal{num}ShouldFlipCopy", props);
+                decalBlendMode[i] = FindProperty($"_Decal{num}BlendMode", props);
+                decalTex_UVMode[i] = FindProperty($"_Decal{num}Tex_UVMode", props);
+                decalSyncScale[i] = FindProperty($"_Decal{num}SyncScale", props);
+            }
         }
 
         protected override void DrawCustomProperties(Material material)
@@ -37,14 +78,105 @@ namespace lilToon
             // customBox        box (similar to unity default box)
             // customToggleFont label for box
 
-            isShowCustomProperties = Foldout("Custom Properties", "Custom Properties", isShowCustomProperties);
-            if(isShowCustomProperties)
+            // Decal Count Control at Top
+            EditorGUILayout.BeginVertical(boxOuter);
+            EditorGUILayout.LabelField("Decal Count Control", customToggleFont);
+            EditorGUILayout.BeginVertical(boxInnerHalf);
+            
+            EditorGUI.BeginChangeCheck();
+            m_MaterialEditor.ShaderProperty(decalCount, "Decal Count (1-10)");
+            if(EditorGUI.EndChangeCheck())
+            {
+                // Clamp value to 1-10
+                decalCount.floatValue = Mathf.Clamp(decalCount.floatValue, 1, MAX_DECALS);
+            }
+            
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndVertical();
+
+            // Get current decal count
+            int currentDecalCount = Mathf.RoundToInt(decalCount.floatValue);
+
+            // Draw decal sections dynamically based on count
+            for(int i = 0; i < currentDecalCount; i++)
+            {
+                int num = i + 1;
+                DrawDecalSection(material, i, num);
+            }
+        }
+
+        private void DrawDecalSection(Material material, int index, int displayNum)
+        {
+            string sectionName = $"Decal {displayNum}";
+            isShowDecal[index] = Foldout(sectionName, sectionName, isShowDecal[index]);
+            
+            if(isShowDecal[index])
             {
                 EditorGUILayout.BeginVertical(boxOuter);
-                EditorGUILayout.LabelField(GetLoc("Custom Properties"), customToggleFont);
+                EditorGUILayout.LabelField(sectionName, customToggleFont);
                 EditorGUILayout.BeginVertical(boxInnerHalf);
 
-                //m_MaterialEditor.ShaderProperty(customVariable, "Custom Variable");
+                m_MaterialEditor.ShaderProperty(useDecal[index], $"Use Decal {displayNum}");
+                if(useDecal[index].floatValue > 0)
+                {
+                    EditorGUI.indentLevel++;
+                    m_MaterialEditor.TexturePropertySingleLine(new GUIContent("Texture"), decalTex[index], decalColor[index]);
+                    
+                    // Scale同期トグル
+                    m_MaterialEditor.ShaderProperty(decalSyncScale[index], "Sync Scale X/Y");
+                    
+                    // 同期モード時：カスタムScaleスライダーを表示
+                    if(decalSyncScale[index].floatValue == 1.0f)
+                    {
+                        EditorGUI.BeginChangeCheck();
+                        Vector4 st = decalTex_ST[index].vectorValue;
+                        float syncedScale = EditorGUILayout.Slider("Scale", st.x, -1.0f, 1.0f);
+                        if(EditorGUI.EndChangeCheck())
+                        {
+                            st.x = syncedScale;
+                            st.y = syncedScale;
+                            decalTex_ST[index].vectorValue = st;
+                        }
+                    }
+                    
+                    EditorGUI.BeginChangeCheck();
+                    lilEditorGUI.UV4Decal(
+                        m_MaterialEditor,
+                        decalIsDecal[index],
+                        decalIsLeftOnly[index],
+                        decalIsRightOnly[index],
+                        decalShouldCopy[index],
+                        decalShouldFlipMirror[index],
+                        decalShouldFlipCopy[index],
+                        decalTex[index],
+                        decalTex_SR[index],
+                        decalTexAngle[index],
+                        decalDecalAnimation[index],
+                        decalDecalSubParam[index],
+                        decalTex_UVMode[index]
+                    );
+                    // UV4Decalの初期化ボタンが押されたとき、追加プロパティもリセット
+                    if(EditorGUI.EndChangeCheck())
+                    {
+                        // Scale同期: UV4Decal内でXが変更されたらYも同じ値に
+                        if(decalSyncScale[index].floatValue == 1.0f)
+                        {
+                            Vector4 st = decalTex_ST[index].vectorValue;
+                            st.y = st.x;
+                            decalTex_ST[index].vectorValue = st;
+                        }
+                        
+                        if(decalIsDecal[index].floatValue == 0.0f && decalTex_UVMode[index].floatValue == 0.0f)
+                        {
+                            decalBlendMode[index].floatValue = 0.0f;
+                            decalColor[index].colorValue = Color.white;
+                            decalTex_ST[index].vectorValue = new Vector4(1, 1, 0, 0);
+                        }
+                    }
+                    
+                    m_MaterialEditor.ShaderProperty(decalBlendMode[index], "Blend Mode");
+                    EditorGUI.indentLevel--;
+                }
 
                 EditorGUILayout.EndVertical();
                 EditorGUILayout.EndVertical();
